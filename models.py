c1b5c99ea460297a1cd6cbc62f8baed7c21e4164
Johnny <johnny.liang@ontariotechu.net>
2025-03-29 12:10:18 -0400

    Linear Model trained with 3 epocs, saved in saved_models

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ import torch
 
 class LinearModel(nn.Module):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         # 32*32*3 corresponds to the size/shape of img. 
         # W=32 ; H=32 ; 3 Color image ; 
         # output of 10 corresponding to the 10 classes in CIFAR10
-        self.fc == nn.Linear(32*32*3, 10)
+        self.fc = nn.Linear(32*32*3, 10)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def imshowdef(img):
-    img = img / 2 + 0.5
+    img = img + 2
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
