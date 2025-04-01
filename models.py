66a23cc683750181b61723dbf476271f0e2da048
Johnny <johnny.liang@ontariotechu.net>
2025-03-31 23:47:50 -0400

    resnet model added and trained

--- models.py
+++ models.py
@@ -61,16 +61,16 @@ class ConvolutionNetwork(nn.Module):
     
 # CNN with Batch Normalization
 class ConvolutionBatchNetwork(nn.Module):
-    def __init__(self, in_chan, out_chan, kernel_size, stride=1, padding="same", input_size=32):
+    def __init__(self, in_chan, out_chan, kernel_size, stride=1, padding="same", input_size=32, bias=True):
         super().__init__()
         
         self.model = nn.Sequential(
-            nn.Conv2d(in_chan, out_chan*4, kernel_size, stride, padding, bias=True),
+            nn.Conv2d(in_chan, out_chan*4, kernel_size, stride, padding, bias=bias), # Want to try with bias = False | The current save is with bias = True
             nn.BatchNorm2d(out_chan*4),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            nn.Conv2d(in_chan*4, out_chan*8, kernel_size, stride, padding, bias=True),
+            nn.Conv2d(out_chan*4, out_chan*8, kernel_size, stride, padding, bias=bias),
             nn.BatchNorm2d(out_chan*8),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
@@ -84,3 +84,45 @@ class ConvolutionBatchNetwork(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+class ResNet(nn.Module):
+    def __init__(self, in_chan, out_chan, kernel_size, stride = 1, padding = "same", ):
+        super().__init__()
+
+        self.learning_block = nn.Sequential(
+            nn.Conv2d(in_chan, out_chan*4, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_chan*4),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # At this point, I: 16*16 || C: 12
+
+            nn.Conv2d(out_chan * 4, out_chan*8, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_chan*8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # At this point, I: 8*8 || C: 24
+        )
+
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_chan, out_chan*8, kernel_size=1, stride=4),
+            nn.BatchNorm2d(out_chan * 8)
+        )
+
+        self.middle_block = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(8 * 8 * out_chan * 8, 128),
+        )
+
+        # Resnet Code
+        self.relu = nn.ReLU()
+        self.output_layer = nn.Linear(128, 10)
+
+    def forward(self, x):
+        identity = x
+        identity = self.downsample(identity)  
+        
+        x = self.learning_block(x)            
+        x = self.relu(x + identity)           
+        
+        x = self.middle_block(x)              # Flatten + FC to 128
+        x = self.output_layer(x)              # FC to 10
+        
+        return x
